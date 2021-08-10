35786650c639086225389ec9fadf06af48c61c6d
Naveen Dwivedi <naveen007.1994@gmail.com>
2021-08-10 16:53:50 +0530

    counter configer

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement,increment, set} from "./actions";
+// userselector use to assign the value to the variable 
+// usedispatch use to assign the the action to the dom value
+
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
-
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch()
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={()=>dispatch(increment())}>Increment</button>
+        <button onClick={()=>dispatch(set(0))}>Reset</button>
+        <button onClick={()=>dispatch(decrement())}>Decrement</button>
       </section>
     </main>
   );
