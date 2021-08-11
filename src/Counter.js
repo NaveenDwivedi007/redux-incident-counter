a42c51c051a2177579cdaf770da933f81932369a
Naveen Dwivedi <naveen007.1994@gmail.com>
2021-08-11 13:28:19 +0530

    setcounter setup

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
 import { decrement,increment, set} from "./actions";
+import {SetCounter} from "./SetCounter"
 // userselector use to assign the value to the variable 
 // usedispatch use to assign the the action to the dom value
 
@@ -7,15 +9,19 @@ export const Counter = () => {
   const incident = 'Incident';
   const count = useSelector(state => state.count)
   const dispatch = useDispatch()
+
+
+  const actions = bindActionCreators({increment,decrement,set},dispatch)
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
         <button onClick={()=>dispatch(increment())}>Increment</button>
-        <button onClick={()=>dispatch(set(0))}>Reset</button>
+        <button onClick={()=>actions.set(0)}>Reset</button>
         <button onClick={()=>dispatch(decrement())}>Decrement</button>
       </section>
+      <SetCounter/>
     </main>
   );
 };
--- src/SetCounter.js
+++ src/SetCounter.js
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { set } from "./actions";
+
 export const SetCounter = () => {
+  const storeCount =useSelector(state=>state.count)
+  const update = useDispatch()
+  const [NewCount,setNewCount] = useState(storeCount)
+  useEffect(()=>{
+    setNewCount(storeCount)
+  },[storeCount])
   return (
     <section className="controls">
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          update(set(NewCount))
+          setNewCount(0)
         }}
       >
         <label htmlFor="set-to">Set Count</label>
-        <input id="set-to" type="number" />
-        <input type="submit" />
+        <input id="set-to" type="number" value={NewCount}  onChange={(event)=>{return setNewCount(event.target.value)}}/>
+        <input type="submit"/>
       </form>
     </section>
   );
